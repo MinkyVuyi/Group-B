672dcf80f22669d5595fbb177b5bc5ba1718d0b2
MinkyVuyi <dialevuyi@gmail.com>
2023-10-18 08:17:16 +0200

    Update mongodb.js

--- pages/api/mongodb.js
+++ pages/api/mongodb.js
@@ -1,10 +1,71 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+// import { MongoClient, ServerApiVersion } from "mongodb";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const connectionString = process.env.MONGODB_CONNECTION_STRING;
+
+// const client = new MongoClient(connectionString, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// export async function run(page) {
+//   try {
+//     await client.connect();
+
+//     const db = client.db("devdb");
+//     await client.db("devdb").command({ ping: 1 });
+//     const collection = db.collection("recipes");
+//     const skip = (page - 1) * 100;
+
+//     const data = await collection.find({}).skip(skip).limit(100).toArray();
+//     return data;
+//   } catch (error) {
+//     console.error("Failed to connect to MongoDB:", error);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// export async function run1() {
+//   try {
+//     await client.connect();
+
+//     const db = client.db("devdb");
+//     await client.db("devdb").command({ ping: 1 });
+//     const collection = db.collection("allergens");
+
+//     const data = await collection.find({}).toArray();
+//     const dataArray = data.map((doc) => doc.allergens);
+
+//     return dataArray;
+//   } catch (error) {
+//     console.error("Failed to connect to MongoDB:", error);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// export async function getRecipeById(id) {
+//   await client.connect();
+//   const db = client.db("devdb");
+
+//   const collection = db.collection("recipes");
+//   return collection.findOne({ _id: id });
+// }
+
+// mongodb.js
+import { ServerApiVersion } from "mongodb";
+import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const connectionString = process.env.MONGODB_CONNECTION_STRING;
-
 const client = new MongoClient(connectionString, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -13,10 +74,11 @@ const client = new MongoClient(connectionString, {
   },
 });
 
+
+
 export async function run(page) {
   try {
-    await client.connect();
-
+    await connectClient();
     const db = client.db("devdb");
     await client.db("devdb").command({ ping: 1 });
     const collection = db.collection("recipes");
@@ -26,15 +88,13 @@ export async function run(page) {
     return data;
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
-  } finally {
-    await client.close();
+    throw error;
   }
 }
 
 export async function run1() {
   try {
-    await client.connect();
-
+    await connectClient();
     const db = client.db("devdb");
     await client.db("devdb").command({ ping: 1 });
     const collection = db.collection("allergens");
@@ -45,15 +105,18 @@ export async function run1() {
     return dataArray;
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
-  } finally {
-    await client.close();
+    throw error;
   }
 }
 
 export async function getRecipeById(id) {
-  await client.connect();
-  const db = client.db("devdb");
-
-  const collection = db.collection("recipes");
-  return collection.findOne({ _id: id });
+  try {
+    await connectClient();
+    const db = client.db("devdb");
+    const collection = db.collection("recipes");
+    return collection.findOne({ _id: id });
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+    throw error;
+  }
 }
